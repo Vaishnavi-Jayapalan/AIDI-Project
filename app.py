13771bfaca6ecbfb5db362b5e575ec352a9db880
Vaishnavi J <vaishujayapalan@gmail.com>
2023-03-23 20:01:57 -0400

    Prediction Code Changes

--- app.py
+++ app.py
@@ -1,44 +1,109 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Efficientnetb0 imports
 from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input
+#from tensorflow.keras.applications.efficientnet import preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
-import numpy as np
-
-# Define the custom layer
-class FixedDropout(tf.keras.layers.Dropout):
-    def _get_noise_shape(self, inputs):
-        if self.noise_shape is None:
-            return self.noise_shape
-
-        symbolic_shape = tf.keras.backend.shape(inputs)
-        noise_shape = [symbolic_shape[axis] if shape is None else shape
-                       for axis, shape in enumerate(self.noise_shape)]
-        return tuple(noise_shape)
-
-# Register the custom layer with Keras
-custom_objects = {'FixedDropout': FixedDropout}
-
-# Load the model using the custom objects
-model = load_model('models/EfficientnetB0.h5', custom_objects=custom_objects)
-
+# Define a flask app
 app = Flask(__name__)
 
-@app.route('/')
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/EfficientnetB0.h5'
+
+# Load your trained model
+#model = load_model(MODEL_PATH)
+# Load the model and make a prediction
+custom_objects = {'FixedDropout': tf.keras.layers.Dropout}  # Add the custom object
+model = tf.keras.models.load_model(MODEL_PATH, custom_objects=custom_objects)
+#model._make_predict_function()          # Necessary
+# print('Model loaded. Start serving...')
+
+# Load the list of species names
+species_path="class_info/name_of_spesies.txt"
+with open(species_path, 'r') as f:
+    species_list = [line.strip() for line in f]
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    #img = image.load_img(img_path, target_size=(256, 256))
+    # Load the image
+    print("In model_predict function")
+    img = tf.keras.preprocessing.image.load_img(img_path, target_size=(256, 256))
+    x = tf.keras.preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    train_datagen= ImageDataGenerator(rescale=1.0/255)
+    x = train_datagen.flow(x, shuffle=False).next()
+    # Preprocessing the image
+    #x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    #x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x, mode='caffe')
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
 def index():
+    # Main page
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    img = request.files['image']
-    img = image.load_img(img, target_size=(256, 256))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    preds = model.predict(x)
-    pred_class = preds.argmax(axis=-1)
-    return render_template('index.html', prediction=pred_class[0])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+#        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+#        result = str(pred_class[0][0][1])               # Convert to string
+        # Get the predicted class name
+        pred_class_idx = np.argmax(preds)
+        pred_class = species_list[pred_class_idx]
+        result = str(pred_class)
+        return result
+    return None
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
